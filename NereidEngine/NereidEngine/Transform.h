2ed4f77a222cf4d54fc246a36e60acbcaa027d76
RubenTipparach <santipab.stephane@gmail.com>
2017-03-25 23:41:21 -0500

    blocking out the light stuff

--- NereidEngine/NereidEngine/Transform.h
+++ NereidEngine/NereidEngine/Transform.h
@@ -1,7 +1,26 @@
 #pragma once
+#include <iostream>
+#include "stdafx.h"
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/random.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 class Transform
 {
 public:
+	glm::vec3 localPosition;
+	glm::vec3 globalPosition;
+
+	glm::vec3 scale;
+
+	glm::vec3 eulerRotation;
+
 	Transform();
 	~Transform();
 };
